f9786e5cee095e5e2a87566696ff0a343fa77e67
jelly <jelly_jl1027@sina.com>
2018-01-23 15:26:49 +0800

    cart 增 减 删 页面完成--jelly 2018.1.23

---
+++ src/main/webapp/index/js/tops.js
@@ -0,0 +1,14 @@
+/*
+tops页面的go跳转*/
+
+function go(type,obj) {
+    $(document).ready(function () {
+        var goValue= $("#hotgo").val();
+        if(isNaN(goValue)){
+            $(obj).attr("href","/tops?type="+type+"&pageNum=1");
+        }else{
+            $(obj).attr("href","/tops?type="+type+"&pageNum="+parseInt(goValue));
+        }
+    });
+
+}
