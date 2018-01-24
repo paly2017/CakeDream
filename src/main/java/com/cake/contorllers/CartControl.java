b77b0bcd0ccc11d935070e34179ce3f89feb142c
jelly <jelly_jl1027@sina.com>
2018-01-24 23:49:50 +0800

    个人中心完成--jelly 2018.1.24

--- src/main/java/com/cake/contorllers/CartControl.java
+++ src/main/java/com/cake/contorllers/CartControl.java
@@ -3,7 +3,7 @@ package com.cake.contorllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
-public class cartControl {
+public class CartControl {
     @RequestMapping("/cart")
     public String jumpCart(){
         return "index/cart";
